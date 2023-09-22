91398ecf3b03ee527e109ff4f38f544ca778a348
João Vitor Miguez da Cunha <PET0147543@aluno.firjansenaisesi.com.br>
2023-09-21 21:22:56 -0300

    Trabalho quase finalizado

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 //ToDo-List
 alert("BEM VINDO À SUA ToDO-list")
-let lista = [];
-let listaCompleta = "";
+let lista01 = [];
+let resultadoLista01 = "";
+let lista02 = [];
+let resultadoLista02 = "";
 let lixeira = [];
-let lixeiraCompleta = " ";
+let resultadoLixeira = "";
 
 
-function menu(){
+function menu() {
     let escolha;
 
-    while(escolha != 0){
-        escolha = parseInt(window.prompt(" - Menu - \n1 - Adcionar tarefa\n2 - Ver tarefas\n3 - Excluir tarefa\n4 - ver lixeira\n0-sair"))
-        
+    while (escolha != 0) {
+        escolha = parseInt(window.prompt(" - Menu - \n1 - Adcionar tarefa\n2 - Ver listas\n3 - Excluir tarefa\n4 - ver lixeira\n5-Buscar tarefa deletada"))
+
         switch (escolha) {
             case 1:
                 addTarefa();
@@ -24,6 +26,9 @@ function menu(){
                 break;
             case 4:
                 verLixeira();
+                break;
+            case 5:
+                retornaTarefa();
                 break;
             case 0:
                 alert("Saindo :(")
@@ -38,46 +43,169 @@ function menu(){
 
 }
 
-function addTarefa(){
+
+function addTarefa() {
     let digiteTarefa;
+    let escolhaLista
+    
+    while (true) {
+        escolhaLista = parseInt(prompt("Qual a lista deseja acessar:\n 1 - lista 01\n 2 - lista 02"))
+        digiteTarefa = prompt ("digite qual tarefa quer adicionar")
+        switch (escolhaLista) {
+            case 1:
+                lista01.push(digiteTarefa)
+                alert("Tarefa adcionada a lista 01!!")
+                menu();
+                break;
 
-    digiteTarefa = window.prompt("Digite a tarefa que deseja inserir à lista: ")
-    lista.push(digiteTarefa)
-    alert("Tarefa adcionada a lista!!")
+            case 2:
+                lista02.push(digiteTarefa)
+                alert("Tarefa adcionada a lista 02!!")
+                menu();
+                break;
 
-}
-
-function listaItens(){
-    listaCompleta = "";
-    for (let i = 0; i < lista.length ; i++){
-        listaCompleta = listaCompleta + (i+1) + "-" + lista[i]  + "\n"
+            default:
+                break;
+        }
     }
-    return listaCompleta;
+
+
 }
 
-function deletaTarefa(){
+function listaItens() {
+    let resultado
+
+     // Limpa a lista resultante ao entrar na função
+     resultadoLista01 = "";
+     resultadoLista02 = "";
+
+    while (true) {
+         resultado = parseInt(prompt("Qual lista você deseja exibir:\n 1 - lista 01\n 2 - lista 02"))
+         switch (resultado) {
+            case 1:
+                for (let i = 0; i < lista01.length; i++) {
+                resultadoLista01 = resultadoLista01 + (i + 1) + "-" + lista01[i] + "\n"
+            }
+            return resultadoLista01;
+                
+
+            case 2:    
+                for (let i = 0; i < lista02.length; i++) {
+                resultadoLista02 = resultadoLista02 + (i + 1) + "-" + lista02[i] + "\n"
+            }
+            return resultadoLista02;
+                
+         
+            default:
+                alert ("escolha uma opção válida!")
+                break;
+         }
+      
+}
+}
+
+function deletaTarefa() {
     let deletaItem;
+    let escolhaItem;
 
-    deletaItem = parseInt(window.prompt("Qual tarefa você deseja deletar? " + "\n" + listaItens()))
-    
-    if((deletaItem < 1) || (deletaItem > lista.length)){
-        alert("Insira um item existente!!")
-        deletaTarefa();
-    }else{
-        lista.splice((deletaItem - 1), 1);
-        listaCompleta = "";
-        alert("Tarefa deletada!! ");
-        menu();
+     // Limpa a lista resultante ao entrar na função
+     resultadoLista01 = "";
+     resultadoLista02 = "";
+
+    deletaItem = parseInt(window.prompt("De qual lista deseja deletar?\n1 - Lista 01\n2 - Lista 02"))
+
+    switch (deletaItem) {
+        case 1:
+            // Constrói a lista 01 antes de mostrar para o usuário
+            for (let i = 0; i < lista01.length; i++) {
+                resultadoLista01 += (i + 1) + "-" + lista01[i] + "\n";
+            }
+
+            alert(resultadoLista01);
+            escolhaItem = parseInt(prompt("Escolha o item que deseja excluir: "));
+            if (escolhaItem >= 1 && escolhaItem <= lista01.length) {
+                let tarefaRemovida = lista01.splice(escolhaItem - 1, 1)[0];
+
+                // Adiciona a tarefa à lixeira
+                lixeira.push(tarefaRemovida);
+                alert("Tarefa deletada e movida para a lixeira, meu brother!!");
+            } else {
+                alert("Insira um item existente!!");
+            }
+            break;
+
+        case 2:
+            // Constrói a lista 02 antes de mostrar para o usuário
+            for (let i = 0; i < lista02.length; i++) {
+                resultadoLista02 += (i + 1) + "-" + lista02[i] + "\n";
+            }
+
+            alert(resultadoLista02);
+            escolhaItem = parseInt(prompt("Escolha o item que deseja excluir: "));
+            if (escolhaItem >= 1 && escolhaItem <= lista02.length) {
+                let tarefaRemovida = lista02.splice(escolhaItem - 1, 1)[0];
+
+                // Adiciona a tarefa à lixeira
+                lixeira.push(tarefaRemovida);
+            } else {
+                alert("Insira um item existente!!");
+            }
+            break;
+
+        default:
+            alert("Insira uma lista válida!!");
+            break;
     }
-    
+
 }
- function verLixeira(){
 
-    lixeira.push(deletaItem)
+function verLixeira() {
 
-    alert (lixeira)
-    
+    resultadoLixeira = "";
 
- }
+    if((lixeira.length == 0)){
+        alert("A lixeira está vazia")
+    }else{
+        for (let i = 0; i < lixeira.length; i++) {
+            resultadoLixeira += (i + 1) + "-" + lixeira[i] + "\n";
+        }
+        alert("Itens na lixeira:\n" + resultadoLixeira);
+    }
+
+
+}
+
+function retornaTarefa(){
+    let escolhaLixeira;
+    let escolhaLista;
+
+    if (lixeira.length === 0) {
+        alert("A lixeira está vazia");
+    } else {
+        let lixeiraItens = "";
+
+        for (let i = 0; i < lixeira.length; i++) {
+            lixeiraItens += (i + 1) + "-" + lixeira[i] + "\n";
+        }
+
+        escolhaLixeira = parseInt(window.prompt("Itens na lixeira:\n" + lixeiraItens + "Escolha o item que deseja retornar à lista: "));
+
+        if (escolhaLixeira >= 1 && escolhaLixeira <= lixeira.length) {
+            escolhaLista = parseInt(window.prompt("Qual lista deseja adicionar o item de volta:\n1 - Lista 01\n2 - Lista 02"));
+
+            if (escolhaLista === 1) {
+                lista01.push(lixeira.splice(escolhaLixeira - 1, 1)[0]);
+                alert("Item retornado à Lista 01!");
+            } else if (escolhaLista === 2) {
+                lista02.push(lixeira.splice(escolhaLixeira - 1, 1)[0]);
+                alert("Item retornado à Lista 02!");
+            } else {
+                alert("Escolha uma lista válida!");
+            }
+        } else {
+            alert("Escolha um item válido da lixeira!");
+        }
+    }
+}
 
 menu();
